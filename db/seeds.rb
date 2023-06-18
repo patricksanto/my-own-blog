c4b3411f36132db56ea6920dcc0c643e41436df9
Patrick Santos <patrick.sg.santos@gmail.com>
2023-06-18 11:13:39 -0700

    all devise configuration done for a simple user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: 'patrick.sg.santos@gmail.com').first_or_initialize
+user.update!(
+  password: '123456',
+  password_confirmation: '123456'
+)
