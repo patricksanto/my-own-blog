1a92e057b680be01b426e5adeb9d997b8a250348
Patrick Santos <patrick.sg.santos@gmail.com>
2023-06-18 15:34:59 -0700

    updates in the articles model, create scopes to draft, published and scheduled

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 20 }, uniqueness: true
   validates :body, presence: true, length: { minimum: 50 }
+
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where('published_at <= ?', Time.current) }
+  scope :schedule, -> { where('published_at > ?', Time.current) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_175053) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_222826) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "callback"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
